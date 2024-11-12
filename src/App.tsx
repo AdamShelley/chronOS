888288903a356729f26ce27fcbe5e373c1458216
Adam Shelley <adam.shelley18@gmail.com>
2024-11-12 19:10:36 +0800

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { AppMonitor } from "./monitor";
+
+function App() {
+  return (
+    <main className="container">
+      <p>Test</p>
+      <AppMonitor />
+    </main>
+  );
+}
+
+export default App;
